ba2602e8f41ca3555cef4fb676a7046aa41cbc4f
henriquepadua <henrique.lommes@gmail.com>
2024-04-28 18:45:44 -0300

    Adicionando Desafio de conversão de moedas

--- android/app/src/main/kotlin/com/example/camp_2024/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.camp_2024
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:camp_2024/pages/LoginPage.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home : LoginPage()));
-}
