08b18c78589d79d1389b870ebdf918a6075fd908
henriquepadua <henrique.lommes@gmail.com>
2024-04-27 21:25:32 -0300

    corrigindo overflow na tela de login

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -17,81 +17,83 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       backgroundColor: const Color(0xFFC1007E),
       body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 250),
-                  child: Column(children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Image.asset("assets/logo_ioasys.png"),
-                      ],
-                    ),
-                    const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 80)),
-                    const Text(
-                      "Seja bem vind@!",
-                      style: TextStyle(color: Colors.white, fontSize: 32),
-                    ),
-                    const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 10)),
-                    const Text("Lista de tarefas",
-                        style: TextStyle(color: Colors.white, fontSize: 16)),
-                    const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 10)),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 80.0, vertical: 10),
-                      child: TextFormField(
-                        textAlign: TextAlign.center,
-                        decoration: const InputDecoration(
-                          hintText: "Digite seu email",
-                          labelStyle: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15.0,
+        child: SingleChildScrollView(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 250),
+                    child: Column(children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Image.asset("assets/logo_ioasys.png"),
+                        ],
+                      ),
+                      const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 80)),
+                      const Text(
+                        "Seja bem vind@!",
+                        style: TextStyle(color: Colors.white, fontSize: 32),
+                      ),
+                      const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 10)),
+                      const Text("Lista de tarefas",
+                          style: TextStyle(color: Colors.white, fontSize: 16)),
+                      const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 10)),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 80.0, vertical: 10),
+                        child: TextFormField(
+                          textAlign: TextAlign.center,
+                          decoration: const InputDecoration(
+                            hintText: "Digite seu email",
+                            labelStyle: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15.0,
+                            ),
+                            border: OutlineInputBorder(),
                           ),
-                          border: OutlineInputBorder(),
                         ),
                       ),
-                    ),
-                    const Padding(padding: EdgeInsets.symmetric(vertical: 2)),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 80.0),
-                      child: TextFormField(
-                        controller: senhaController,
-                        textAlign: TextAlign.center,
-                        decoration: const InputDecoration(
-                          hintText: "Digite sua senha",
-                          labelStyle: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15.0,
+                      const Padding(padding: EdgeInsets.symmetric(vertical: 2)),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 80.0),
+                        child: TextFormField(
+                          controller: senhaController,
+                          textAlign: TextAlign.center,
+                          decoration: const InputDecoration(
+                            hintText: "Digite sua senha",
+                            labelStyle: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15.0,
+                            ),
+                            border: OutlineInputBorder(),
                           ),
-                          border: OutlineInputBorder(),
                         ),
                       ),
-                    ),
-                    const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
-                    OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                        elevation: 20,
-                        backgroundColor: Colors.black,
+                      const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+                      OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          elevation: 20,
+                          backgroundColor: Colors.black,
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const HomePage()),
+                          );
+                        },
+                        child: const Text(
+                          'Entrar',
+                          style: TextStyle(fontSize: 32, color: Colors.white),
+                        ),
                       ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const HomePage()),
-                        );
-                      },
-                      child: const Text(
-                        'Entrar',
-                        style: TextStyle(fontSize: 32, color: Colors.white),
-                      ),
-                    ),
-                  ]))
-            ]),
+                    ]))
+              ]),
+        ),
       ),
     );
   }
